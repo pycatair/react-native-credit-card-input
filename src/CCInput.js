72278232cfe63d8d903de20175a043c07f4d3667
pycatair <pycatair@gmail.com>
2018-03-16 19:55:13 +0900

    add returnKeyType

--- src/CCInput.js
+++ src/CCInput.js
@@ -79,6 +79,7 @@ export default class CCInput extends Component {
           <TextInput ref="input"
             {...additionalInputProps}
             keyboardType={keyboardType}
+            returnKeyType='done'
             autoCapitalise="words"
             autoCorrect={false}
             style={[
